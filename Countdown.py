85f7d3387b2fe68a4cf1d1ae2702fe0e2f281778
wbui3 <buiquangwilliam@yahoo.com>
2022-04-04 10:24:53 -0700

    Update Countdown.py
    Added error checking for dates

--- Countdown.py
+++ Countdown.py
@@ -4,8 +4,13 @@ def countdown(yr,mo,dy):
     currDate = date.today()
     targetDate = date(yr,mo,dy) # 2022,7,30
     daysRemaining = targetDate - currDate
-    print("The date today is ", currDate.month, currDate.day, currDate.year)
-    print("The target date is ", targetDate.month, targetDate.day, targetDate.year)
-    print("Days left:" + str(daysRemaining.days))
+   if int(daysRemaining.days) < 0:
+        print("The date has already passed")
+    elif int(daysRemaining.days) == 0:
+        print("The deadline is today!")
+    else:
+        print("The date today is ", currDate.month, currDate.day, currDate.year)
+        print("The target date is ", targetDate.month, targetDate.day, targetDate.year)
+        print("Days left: " + str(daysRemaining.days))
 
 countdown(2022,7,30)
