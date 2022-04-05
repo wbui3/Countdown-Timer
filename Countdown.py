736080322382bd914fb46d9969340da5812e29a6
wbui3 <buiquangwilliam@yahoo.com>
2022-04-05 11:00:46 -0700

    Update Countdown.py
    error checking for invalid dates

--- Countdown.py
+++ Countdown.py
@@ -2,6 +2,19 @@ from datetime import date
 
 def countdown(yr,mo,dy):
     currDate = date.today()
+    ############ Error checking ############
+    months31=[1, 3, 5, 7, 8, 10, 12]
+    months30 = [4, 6, 9, 11]
+    if (mo == 2 and dy > 28 and yr%4 > 0):
+        print("Invalid february dates. Exiting")
+        return
+    elif (mo in months31 and dy > 31):
+        print("Invalid dates for months with 31. Exiting")
+        return
+    elif (mo in months30 and dy > 30):
+        print("Invalid dates for months with 30. Exiting")
+        return
+    ############ Error checking ############
     targetDate = date(yr,mo,dy) # 2022,7,30
     daysRemaining = targetDate - currDate
    if int(daysRemaining.days) < 0:
